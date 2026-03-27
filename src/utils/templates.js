ba63cb1d80d1d788074344c55259a143c273a48c
aman-kh-1500 <aman.external@gudmed.in>
2026-03-27 12:58:33 +0530

    Updated images: used better quality webp images

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -5,24 +5,24 @@ export const FORMS = {
     name: 'General OP - Form',
     description: 'General outpatient form',
     images: [
-      '/images/generalOP/gen_1.png',
-      '/images/generalOP/gen_2.png',
-      '/images/generalOP/gen_3.png',
+      '/images/generalOP/gen_1.webp',
+      '/images/generalOP/gen_2.webp',
+      '/images/generalOP/gen_3.webp',
       '/images/extra_page.png'
     ],
-    thumbnail: '/images/generalOP/gen_1.png',
+    thumbnail: '/images/generalOP/gen_1.webp',
   },
   'cardiology-op-form': {
     id: 'cardiology-op-form',
     name: 'Cardiology OP - Form',
     description: 'Cardiology outpatient form',
     images: [
-      '/images/cardioOP/cardio_1.png',
-      '/images/cardioOP/cardio_2.png',
-      '/images/cardioOP/cardio_3.png',
+      '/images/cardioOP/cardio_1.webp',
+      '/images/cardioOP/cardio_2.webp',
+      '/images/cardioOP/cardio_3.webp',
       '/images/extra_page.png'
     ],
-    thumbnail: '/images/cardioOP/cardio_1.png',
+    thumbnail: '/images/cardioOP/cardio_1.webp',
   },
 }
 
@@ -33,14 +33,14 @@ export const TEMPLATES = {
     name: 'General OP',
     description: 'General outpatient templates',
     forms: ['general-op-form', 'cardiology-op-form'],
-    thumbnail: '/images/generalOP/gen_1.png',
+    thumbnail: '/images/generalOP/gen_1.webp',
   },
   'cardiology-op': {
     id: 'cardiology-op',
     name: 'Cardiology OP',
     description: 'Cardiology outpatient templates',
     forms: ['cardiology-op-form', 'general-op-form'],
-    thumbnail: '/images/cardioOP/cardio_1.png',
+    thumbnail: '/images/cardioOP/cardio_1.webp',
   },
 }
 
